af615a9ce363898d52eff5c19935623d37d4ed4a
zhaoyj <1650432983@qq.com>
2021-01-26 18:01:47 +0800

    增加框选逻辑

--- design/code/engine.js
+++ design/code/engine.js
@@ -1,3 +1,4 @@
+//座位
 class Rect {
     constructor(ctx, {
         top = 0,
@@ -16,10 +17,13 @@ class Rect {
         this.rectId = 0 //唯一标识
     }
 
-    adjust(left, top, type) {
+    adjust(left, top) {
         this.left += left
         this.top += top
-        this.background = type === "up" ? "#f4a41e" : "red"
+    }
+
+    setSelected(sel) {
+        this.background = sel ? "red" : "#f4a41e"
     }
 
     isPointIn(x, y) {
@@ -56,6 +60,36 @@ class Rect {
     }
 }
 
+//选中区域
+class SelectArea {
+    constructor(ctx, {
+        x1 = 0,
+        y1 = 0,
+        x2 = 0,
+        y2 = 0,
+        background = '#f4a41e'
+    }) {
+        this.ctx = ctx
+        this.x1 = x1
+        this.y1 = y1
+        this.x2 = x2
+        this.y2 = y2
+        this.background = background
+    }
+
+    adjust(x2, y2) {
+        this.x2 = x2
+        this.y2 = y2
+    }
+
+    painting() {
+        this.ctx.save();
+        this.ctx.setLineDash([5])
+        this.ctx.strokeRect(this.x1, this.y1, this.x2 - this.x1, this.y2 - this.y1);
+        this.ctx.restore();
+    }
+}
+
 export class SeatMap {
 
     constructor(canvas_id, seat_span_id, map_status_id, error_hint_id) {
@@ -85,13 +119,19 @@ export class SeatMap {
             this.renderList.push(target)
     }
 
+    addSelectArea(areaConfig) {
+        let target = new SelectArea(this.ctx, areaConfig)
+        this.renderList.push(target)
+        return target
+    }
+
     computeSuitable(rect, leftr = 0, topr = 0) {
         let rectCache = {left: rect.left + leftr, top: rect.top + topr, width: rect.width, height: rect.height}
         console.log(rectCache)
         let suit = true;
         let renderList = this.renderList
         if (rect.rectId) renderList = renderList.filter(ren => {
-            return ren.rectId != rect.rectId
+            return ren.rectId && ren.rectId != rect.rectId
         })
         renderList.forEach((it, idx) => {
             let p1 = {x: it.left, y: it.top}
@@ -136,9 +176,9 @@ export class SeatMap {
     }
 
     addMapEvent() {
-        const that = this
-        let startX, startY, target
+        let startX, startY, target, downEp, area
         this.canvas.addEventListener('mousedown', e => {
+            // debugger
             this.errorHintView.innerText = ""
             startX = e.offsetX, startY = e.offsetY
             if (this.mapStatus == 1) {//如果是添加状态
@@ -146,43 +186,68 @@ export class SeatMap {
                 this.painting()
             } else {
                 if (target != null) {//如果有的话，就反选
-                    target.adjust(0, 0, "up")
-                    this.painting()
+                    target.adjust(0, 0)
+                    target.setSelected(false)
                     target = null
-                } else {//没有的话就设置target
-                    let chooseIdx = null
-                    this.renderList.forEach((it, idx) => {
-                        if (it.isPointIn(startX, startY)) {
-                            chooseIdx = idx
-                        }
-                    })
-                    if (chooseIdx == null)
-                        return
-                    target = that.renderList[chooseIdx]
+                    this.painting()
+                } else if (area != null) {
+                    if (downEp == 1) {
+                        downEp = 2
+                    } else if (downEp == 2) {
+                        deleteArea(this.renderList)
+                        area = null
+                        downEp = null
+                        this.painting()
+                    }
+                } else {
+                    target = getDownRect(this.renderList, startX, startY)
+                    if (target) target.setSelected(true)
+                    downEp = target == undefined ? 1 : 0
+                    this.painting()
                 }
             }
         })
         this.canvas.addEventListener('mousemove', e => {
-            if (this.mapStatus != 1 && target != null) {
-                const currentX = e.offsetX, currentY = e.offsetY
-                if (that.computeSuitable(target, currentX - startX, currentY - startY)) {
-                    target.adjust(currentX - startX, currentY - startY, "move")
+            // debugger
+            const currentX = e.offsetX, currentY = e.offsetY
+            if (this.mapStatus == 1)
+                return
+            if (target != null) {
+                if (this.computeSuitable(target, currentX - startX, currentY - startY)) {
+                    target.adjust(currentX - startX, currentY - startY)
                     startX = currentX, startY = currentY
-                    that.painting()
+                    this.painting()
                 }
+            } else if (downEp == 1) {
+                if (area == null)
+                    area = this.addSelectArea({x1: startX, y1: startY, x2: currentX, y2: currentY})
+                else
+                    area.adjust(currentX, currentY)
+                this.painting()
             }
         })
         this.canvas.addEventListener('mouseup', e => {
+            const currentX = e.offsetX, currentY = e.offsetY
             if (this.mapStatus != 1 && target != null) {
-                const currentX = e.offsetX, currentY = e.offsetY
-                if (that.computeSuitable(target, currentX - startX, currentY - startY)) {
-                    this.errorHintView.innerText = ""
-                    target.adjust(currentX - startX, currentY - startY, "up")
-                    startX = currentX, startY = currentY
-                    that.painting()
-                }
+                this.errorHintView.innerText = ""
+                this.painting()
             }
         })
+
+        function deleteArea(renderList) {
+            let index = renderList.findIndex(ren => {
+                return ren.constructor.name == "SelectArea"
+            })
+            renderList.splice(index, 1);
+        }
+
+        function getDownRect(renderList, x, y) {
+            let clickRect = renderList.find(ren => {
+                if (ren.isPointIn(x, y))
+                    return ren
+            })
+            return clickRect
+        }
     }
 }
 
